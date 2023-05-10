b01af27ec4bf7ad22cd7b705c82713554ea97e79
dannythedeveloper1 <74397260+dannythedeveloper1@users.noreply.github.com>
2023-05-10 15:00:17 -0400

    database deployed

--- server/config/database.js
+++ server/config/database.js
@@ -1,12 +1,14 @@
-const mysql = require('mysql');
+const mysql = require('mysql2');
 
-const pool = mysql.createPool({
-    host: process.env.DB_HOST,
-    user: process.env.DB_USER,
-    password: process.env.DB_PASS,
-    database: process.env.MYSQL_DB,
-    connectionLimit: 10
-});
+// const pool = mysql.createPool({
+//     host: process.env.DB_HOST,
+//     user: process.env.DB_USER,
+//     password: process.env.DB_PASS,
+//     database: process.env.MYSQL_DB,
+//     connectionLimit: 10
+// });
+
+const pool = mysql.createConnection(process.env.DATABASE_URL)
 
 let registration = `CREATE TABLE if not exists registration(
     user_id int auto_increment,
@@ -20,8 +22,7 @@ let profile = `CREATE TABLE if not exists profile(
     user_id int not null,
     first_name varchar(255) not null,
     last_name varchar(255) not null,        
-    PRIMARY KEY (user_profile_id),
-    FOREIGN KEY (user_id) REFERENCES registration(user_id)
+    PRIMARY KEY (user_profile_id)
 )`;
 let question = `CREATE TABLE if not exists question(
     question_id int auto_increment,
@@ -32,8 +33,7 @@ let question = `CREATE TABLE if not exists question(
     post_id varchar(255) not null,
     user_id int not null,
     PRIMARY KEY (question_id),
-    UNIQUE KEY (post_id),
-    FOREIGN KEY (user_id) REFERENCES registration(user_id)
+    UNIQUE KEY (post_id)
 )`;
 let answer = `CREATE TABLE if not exists answer(
     answer_id int auto_increment,
@@ -41,9 +41,7 @@ let answer = `CREATE TABLE if not exists answer(
     answer_code_block varchar(255),
     user_id int not null,
     question_id int not null,
-    PRIMARY KEY (answer_id),
-    FOREIGN KEY (user_id) REFERENCES registration(user_id),
-    FOREIGN KEY (question_id) REFERENCES question(question_id)
+    PRIMARY KEY (answer_id)
 )`;
 pool.query(registration, (err, results) => {
     if (err) throw err;
